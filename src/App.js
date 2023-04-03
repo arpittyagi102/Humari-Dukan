23ad6c449e094b15a702102d07ce6adb6b89a2e2
Arpit <arpittyagirocks@gmail.com>
2023-04-03 20:21:56 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Kuchbhi from "./components/Kuchbhi";
+import "./style.css"
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <Kuchbhi 
+          title="Laptop"
+          image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61d1xNY54HL._AC_UY327_FMwebp_QL65_.jpg"
+          cost={60000}
+        />
+
+        <Kuchbhi 
+          title="Moblie"
+          image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/716fAVud8PL._AC_UY327_FMwebp_QL65_.jpg"
+          cost={114000}
+        />
+
+        <Kuchbhi 
+          title="iPad"
+          image="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61goypdjAYL._AC_UY327_FMwebp_QL65_.jpg"
+          cost={41000}
+        />
+      </div>
+    </>
+  );
+}
---
+++ src/components/Kuchbhi.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./compstyle.css";
+
+export default function Kuchbhi(props){
+    return(
+        <div className="container">
+            <img className="image" src={props.image}/>
+            <div className="inner">
+                <h3>{props.title}</h3>
+                <p>cost:{props.cost}</p>
+            <button style={{marginRight:"10px"}}>Add to cart</button>
+            </div>
+        </div>
+    )
+}
