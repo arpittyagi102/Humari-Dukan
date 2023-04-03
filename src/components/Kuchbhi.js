315af5af518591ca225a4abf7aabf4a597de76d1
Arpit <arpittyagirocks@gmail.com>
2023-04-03 21:43:35 +0530

    fixed the warning to host it on netify : <img> must have an alt

--- src/components/Kuchbhi.js
+++ src/components/Kuchbhi.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./compstyle.css";
 
-export default function Kuchbhi(props){
-    return(
+export default function Kuchbhi(props) {
+    return (
         <div className="container">
-            <img className="image" src={props.image}/>
+            <img className="image" src={props.image} alt="Image not loadingw" />
             <div className="inner">
                 <h3>{props.title}</h3>
                 <p>cost:{props.cost}</p>
-            <button style={{marginRight:"10px"}}>Add to cart</button>
+                <button style={{ marginRight: "10px" }}>Add to cart</button>
             </div>
         </div>
     )
