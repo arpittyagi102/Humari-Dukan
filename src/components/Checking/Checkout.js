100ceb9e4d99dc6275002af943f52dc9a93c87dc
Arpit <arpittyagirocks@gmail.com>
2023-04-24 12:33:50 +0530

    Initial commit for new branch

--- src/components/Checking/Checkout.js
+++ src/components/Checking/Checkout.js
@@ -3,8 +3,8 @@ import './Checkout.css'
 
 export default function Checkout() {
     return(
-        <header className="header-banner">
-            <img className="banner" src="https://cdn.shopify.com/s/files/1/0281/3739/0155/files/checkout-01-01_d3be871f-7415-4646-8314-740f198994ba_2000x.jpg?v=1613791222&quot"/>
-        </header>
+        <>
+
+        </>
     )
 }
