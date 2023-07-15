c20be9d9d2a4720498e3bdc1a9139c852f239a82
kushalnigam0604 <nigamkushal0@gmail.com>
2023-07-15 13:57:54 +0530

    Add Background to counterand change color to green

--- src/components/Products/CartCounter.js
+++ src/components/Products/CartCounter.js
@@ -7,17 +7,17 @@ const CartCounter = ({ state, listOfAddedProduct }) => {
   return (
     <div>
       <button
-        className={`btn btn-danger btn-lg`}
+        className={`btn btn-success btn-lg`}
         onClick={() => dispatch(removefromcart(state.id))}
       >
         -
       </button>
       {listOfAddedProduct.map((item) => {
         if (item.product.id === state.id)
-          return <span className="mx-2" key={item.product.id}>{item.quantity}</span>;
+          return <span className="mx-2 p-2 rounded text-dark bg-light" key={item.product.id}>{item.quantity}</span>;
       })}
       <button
-        className={`btn btn-danger btn-lg`}
+        className={`btn btn-success btn-lg`}
         onClick={() => dispatch(addtocart(state))}
       >
         +
